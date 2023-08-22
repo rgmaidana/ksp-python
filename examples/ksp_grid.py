1a9e9209eeb871a86360d36efb8d7139d478d5d9
rgmaidana <renan.g.maidana@ntnu.no>
2023-08-22 12:46:17 +0200

    Added examples

---
+++ examples/ksp_grid.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+from KSP import KStar
+from KSP.DataStructures import Grid, Edge
+from math import sqrt
+import time
+
+# Find paths s-t from sequences of sidetrack edges
+def getPaths(ks, R):
+    paths = []
+    # The best path found by A* (seq == []) is found first
+    n = str(ks.t).strip('()')
+    path = [n]
+    cost = 0
+    while not n == str(ks.s).strip('()'):
+        n1, n2 = n.split(','); nn = (int(n1), int(n2))
+        cost += ks.AStar.G.weight(ks.AStar.searchTree.T[nn], nn)
+        n = str(ks.AStar.searchTree.T[nn]).strip('()')
+        path.append(n)
+    paths.append([path, cost])
+    R.pop(0)
+
+    # Find the other paths
+    for seq in R:
+        n = str(ks.t).strip('()'); cost = 0
+        path = [n]
+        while not n == str(ks.s).strip('()'):
+            n1, n2 = n.split(','); nn = (int(n1), int(n2))
+            if len(seq) > 0:
+                sidetrack = seq[-1].split(':')[1]
+                u, v = sidetrack.split('),(')
+                u = u.strip('('); v = v.strip(')')
+                if n == v:
+                    u1, u2 = u.split(','); uu = (int(u1), int(u2))
+                    cost += ks.AStar.G.weight(uu, nn)
+                    n = u
+                    path.append(u)
+                    seq.pop(-1)
+                else:
+                    cost += ks.AStar.G.weight(ks.AStar.searchTree.T[nn], nn)
+                    n = str(ks.AStar.searchTree.T[nn]).strip('()')
+                    path.append(n)
+            else:
+                cost += ks.AStar.G.weight(ks.AStar.searchTree.T[nn], nn)
+                n = str(ks.AStar.searchTree.T[nn]).strip('()')
+                path.append(n)
+        paths.append([path, cost])
+    return paths
+
+if __name__ == "__main__":
+    # Define the graph search algorithm
+    # alg = Dijkstra()
+    # alg = AStar(h=get_h)
+    ks = KStar()
+
+    # Initialize grid as problem graph for K*
+    graph = Grid(origin=(0,0), width=10, height=10, resolution=1)
+    
+    numNodes = 1
+
+    # Successor function for A*
+    def successor(u):
+        global numNodes
+        neighbors = graph.neighbors(u)
+        successors = []
+        for v in neighbors:
+            numNodes += 1
+            newEdge = Edge(u=u,v=v,c=1)
+            successors.append(newEdge)
+        return successors
+
+    # Set successor function for A*
+    ks.AStar.successors = successor
+    # ks.AStar.h = lambda v, t: sqrt((v[0]-t[0])**2 + (v[1]-t[1])**2)   # Euclidean distance
+
+    # Get the shortest path tree
+    s, t = graph.origin, (9,9)      # Start and goal depend on the graph notation
+    t1 = time.time()
+    K = 1000000
+    sequences = ks.search(s, t, k=K)
+    print('Time for %d paths: %.4f seconds' % (K, (time.time()-t1)))
+    paths = getPaths(ks, sequences)
+    print('Nodes explored: %d' % numNodes)
+    
+    # Plot paths
+    import matplotlib.pyplot as plt
+    plt.figure()
+    legend = []
+    for i, p in enumerate(paths):
+        xl, yl = [], []
+        path, c = p
+        path.reverse()
+        for loc in path:
+            x, y = loc.split(',')
+            xl.append(int(x)); yl.append(int(y))
+        plt.plot(xl,yl)
+        legend.append('Path %d (%d)' % (i+1, c))
+    # plt.legend(legend)
+    plt.axis([-0.5, 9.5, -0.5, 9.5])
+    plt.show()
---
+++ examples/ksp_successor_riskpath.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+from KSP import KStar
+from KSP.DataStructures import Edge, WeightedGraph
+
+# Find paths s-t from sequences of sidetrack edges
+def getPaths(ks, R):
+    paths = []
+    # The best path found by A* (seq == []) is found first
+    path = [ks.t]; n = ks.t
+    cost = 0
+    while not n == ks.s:
+        cost += ks.AStar.G.weight(ks.AStar.searchTree.T[n], n)
+        n = ks.AStar.searchTree.T[n]
+        path.append(n)
+    paths.append([path, cost-1])
+    R.pop(0)
+
+    # Find the other paths
+    for seq in R:
+        n = ks.t; cost = 0
+        path = [n]
+        while not n == ks.s:
+            if len(seq) > 0:
+                sidetrack = seq[-1].split(':')[1]
+                u, v = sidetrack.split(',')
+                if n == v:
+                    cost += ks.AStar.G.weight(u, n)
+                    n = u
+                    path.append(u)
+                    seq.pop(-1)
+                else:
+                    cost += ks.AStar.G.weight(ks.AStar.searchTree.T[n], n)
+                    n = ks.AStar.searchTree.T[n]
+                    path.append(n)
+            else:
+                cost += ks.AStar.G.weight(ks.AStar.searchTree.T[n], n)
+                n = ks.AStar.searchTree.T[n]
+                path.append(n)
+        paths.append([path, cost-1])
+    return paths
+
+simTree = {
+    'S1':  {'prob': 0/100},
+    'S2': {'prob': 10/100},
+    'S3': {'prob':  5/100},
+    'S4': {'prob': 20/100},
+    'S5': {'prob': 15/100},
+    'S6':  {'prob': 7/100},
+    'S7': {'prob': 25/100},
+    'S8': {'prob': 30/100},
+    'S9': {'prob': 20/100},
+    'S10': {'prob': 8/100},
+    'end': {'prob': 1}
+}
+
+G = {
+    'S1': ['S2', 'S3'],
+    'S2': ['S4', 'S5'],
+    'S3': ['S5', 'S6'],
+    'S4': ['S7', 'S8'],
+    'S5': ['S8', 'S9'],
+    'S6': ['S9', 'S10'],
+    'S7': ['end'],
+    'S8': ['end'],
+    'S9': ['end'],
+    'S10':['end']  
+}
+
+from math import inf
+
+class Node:
+    def __init__(self, name, parent=None, data=None):
+        self.name = name
+        self.parent = parent
+        self.data = data
+
+if __name__ == "__main__":
+    # Define the graph search algorithm
+    # alg = Dijkstra()
+    # alg = AStar(h=get_h)
+    ks = KStar()
+
+    def getW(u):
+        uu = ks.AStar.searchTree.T[u]
+        wu = 1
+        while not uu == None:
+            pc = 0.5*(simTree[uu]['prob']+simTree[u]['prob'])
+            wu = (1-pc)*getW(uu)
+            u = uu
+            uu = ks.AStar.searchTree.T[u]
+        return wu
+
+    def successor(u):
+        newEdges = []
+        neighbors = G[u]
+        if 'end' in neighbors:
+            return [Edge(u=u,v='end',c=1)]
+        for v in neighbors:
+            pu = simTree[u]['prob']; pv = simTree[v]['prob']
+            pc = 0.5*(pu+pv)
+            pp = pc*getW(u)
+            newEdges.append(Edge(u=u, v=v, c=pp))
+        return newEdges
+
+    # Set successor function for A*
+    ks.AStar.successors = successor
+
+    # Functions
+    ks.AStar.cost = lambda u, v: ks.AStar.searchTree.g[u] + ks.AStar.G.weight(u, v)
+    
+    # Get the shortest path tree
+    s, t = 'S1', 'end'      # Start and goal depend on the graph notation
+    sequences = ks.search(s, t, k=inf)
+    paths = getPaths(ks, sequences)
+    
+    # Print paths
+    print()
+    for i, p in enumerate(paths):
+        path, c = p
+        path.reverse()
+        print('Path %d\t- cost %.4f: ' % (i+1, c), end='')
+        for i in range(len(path)-1):
+            print('%s->' % path[i], end='')
+        print('%s' % path[-1])
